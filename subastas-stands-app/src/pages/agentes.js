a43e6ef1b8f789c7fc4744e3bc68481bd597afd8
Diego Flores <dfmonroy07@gmai.com>
2024-06-19 12:33:14 -0600

    updated agentes

--- subastas-stands-app/src/pages/agentes.js
+++ subastas-stands-app/src/pages/agentes.js
@@ -1,61 +1,61 @@
 import React from "react";
-import { ArrowRightIcon } from "@heroicons/react/24/outline";
+import ArchivoBtn from "../assets/images/boton-archivo.webp"
+import Agente1Img from "../assets/images/agentes/agente1.webp"
+import Agente2Img from "../assets/images/agentes/agente2.webp"
+import Agente3Img from "../assets/images/agentes/agente3.webp"
+import Agente4Img from "../assets/images/agentes/agente4.webp"
+import Agente5Img from "../assets/images/agentes/agente5.webp"
+import Agente6Img from "../assets/images/agentes/agente6.webp"
+import Agente7Img from "../assets/images/agentes/agente7.webp"
+import Agente8Img from "../assets/images/agentes/agente8.webp"
 
 const people = [
     {
-        name: 'Agente W',
-        username: 'werevertumorro',
-        imageUrl:
-        'https://pbs.twimg.com/profile_images/1660218462193549313/O_uIodQF_400x400.jpg',
+        name: 'Agente 1',
+        username: 'agente1',
+        imageUrl: Agente1Img,
         active: true
     },
     {
-        name: 'Agente W',
-        username: 'werevertumorro',
-        imageUrl:
-        'https://pbs.twimg.com/profile_images/1660218462193549313/O_uIodQF_400x400.jpg',
+        name: 'Agente 2',
+        username: 'agente2',
+        imageUrl: Agente2Img,
         active: true
     },
     {
-        name: 'Agente W',
-        username: 'werevertumorro',
-        imageUrl:
-        'https://pbs.twimg.com/profile_images/1660218462193549313/O_uIodQF_400x400.jpg',
+        name: 'Agente 3',
+        username: 'agente3',
+        imageUrl: Agente3Img,
         active: true
     },
     {
-        name: 'Agente W',
-        username: 'werevertumorro',
-        imageUrl:
-        'https://pbs.twimg.com/profile_images/1660218462193549313/O_uIodQF_400x400.jpg',
+        name: 'Agente 4',
+        username: 'agente4',
+        imageUrl: Agente4Img,
         active: true
     },
     {
-        name: 'Agente W',
-        username: 'werevertumorro',
-        imageUrl:
-        'https://pbs.twimg.com/profile_images/1660218462193549313/O_uIodQF_400x400.jpg',
+        name: 'Agente 5',
+        username: 'agente5',
+        imageUrl: Agente5Img,
         active: true
     },
     {
-        name: 'Agente W',
-        username: 'werevertumorro',
-        imageUrl:
-        'https://pbs.twimg.com/profile_images/1660218462193549313/O_uIodQF_400x400.jpg',
+        name: 'Agente 6',
+        username: 'agente6',
+        imageUrl: Agente6Img,
         active: true
     },
     {
-        name: 'Agente W',
-        username: 'werevertumorro',
-        imageUrl:
-        'https://pbs.twimg.com/profile_images/1660218462193549313/O_uIodQF_400x400.jpg',
+        name: 'Agente 7',
+        username: 'agente7',
+        imageUrl: Agente7Img,
         active: true
     },
     {
-        name: 'Agente W',
-        username: 'werevertumorro',
-        imageUrl:
-        'https://pbs.twimg.com/profile_images/1660218462193549313/O_uIodQF_400x400.jpg',
+        name: 'Agente 8',
+        username: 'agente8',
+        imageUrl: Agente8Img,
         active: true
     },
   ]
@@ -78,11 +78,11 @@ function Agentes(){
                         <img className="w-full rounded-2xl object-cover" src={person.imageUrl} alt={person.name} />
                         <h3 className="mt-6 text-lg font-semibold leading-8 text-center tracking-tight text-principal">{person.name}</h3>
                         <div className="mt-6 flex items-center justify-center gap-x-6">
-                            <a
+                        <a
                             href={`/agente/${person.username}`}
-                            className="rounded-md bg-gray-900/95 px-3.5 py-2.5 text-sm font-semibold text-principal/80 shadow-sm hover:bg-gray-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+                            className="rounded-md bg-transparent px-3.5 py-2.5 text-sm font-semibold text-principal/80 shadow-sm hover:bg-gray-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
                             >
-                                Archivo del agente <ArrowRightIcon className="inline h-6 w-6"/>
+                                <img src={ArchivoBtn} alt="ver archivo" className="h-20 w-auto"  />
                             </a>
                         </div>
                     </li>
